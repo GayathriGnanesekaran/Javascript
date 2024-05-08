c507e55b1af3aa0b626ae7ce20c39af747246f89
GayathriGnanesekaran <110585404+GayathriGnanesekaran@users.noreply.github.com>
2024-05-08 22:01:26 +0530

    Add files via upload

---
+++ JAVASCRIPT/car.js
@@ -0,0 +1,15 @@
+class car{
+    login(){
+        console.log("you are loggedin")
+    }
+}
+function user(){
+    
+        console.log("you are loggedout")
+    
+}
+function details(){
+        console.log("your details are added")
+}
+export default car
+export{user,details}
---
+++ JAVASCRIPT/ex.js
@@ -0,0 +1,3 @@
+import car,{user,details} from './car.js'
+let car1=new car()
+car1.login()
